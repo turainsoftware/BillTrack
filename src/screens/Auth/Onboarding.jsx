3cb1ed884e22cdbc5b83f350cce05d248123c926
jhantu626 <jhantubala626@gmail.com>
2025-03-25 11:05:31 +0530

    initialized the onboarding

--- src/screens/Auth/Onboarding.jsx
+++ src/screens/Auth/Onboarding.jsx
@@ -1,11 +1,13 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { colors } from '../../util/utils'
 
 const Onboarding = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>Onboarding</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
